8a64b9c0e40f2fb46c20c3fd04bd309a5e1e55b0
YashTiwari0-7 <yashtiwariofficial07@gmail.com>
2026-03-18 03:21:19 +0530

    Server and the Backend Created

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));   
+// location update event from client
+io.on("connection", (socket) => {
+    socket.on("sendLocation", (data) => {
+        io.emit("recievedLocation", {id: socket.id, ...data});
+    });
+    console.log("New client connected");
+});
+
 app.get("/", (req, res) => {
     console.log("Request received for the home page");
-    res.send("Server is running");
+    res.render("index");
 });
 
 server.listen(PORT, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,49 @@
-console.log("Script loaded successfully!"); 
+const socket = io();
+
+
+// Get user's location and send it to the server
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position) => {
+        console.log(position.coords);
+        const {longitude, latitude} = position.coords;
+        
+        socket.emit("sendLocation", {longitude, latitude})
+    }, (error) => {
+        console.error("Error getting location: ", error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+    
+);
+
+}
+
+// Get Map and set view to user's location
+const map = L.map("map").setView([0, 0], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+/*
+// Light
+const light = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png");
+
+// Dark
+const dark = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"); */
+// User location marker
+const markers = {};
+
+socket.on("recievedLocation", (data) => {
+    const {id, longitude, latitude} = data;
+    map.setView([latitude, longitude], 10);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
